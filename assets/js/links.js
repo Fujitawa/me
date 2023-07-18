d886b6924a9d36cd66ae382b8cac15246d99118d
Fujitawa <62254177+Fujitawa@users.noreply.github.com>
2023-07-18 10:16:55 +0200

    Create links.js

---
+++ assets/js/links.js
@@ -0,0 +1,21 @@
+  // Загрузка данных из JSON-файла
+fetch('links.json')
+  .then(response => response.json())
+  .then(data => {
+    const linksList = document.getElementById('links-list');
+    
+    // Очистка списка ссылок
+    linksList.innerHTML = '';
+
+    // Добавление ссылок из JSON
+    data.links.forEach(link => {
+      const listItem = document.createElement('li');
+      const anchor = document.createElement('a');
+      anchor.href = link.url;
+      anchor.target = '_blank';
+      anchor.textContent = link.text;
+      listItem.appendChild(anchor);
+      linksList.appendChild(listItem);
+    });
+  })
+  .catch(error => console.error(error));
