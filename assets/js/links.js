64b7948d9ff1d7b94fdfacc63315521af53cccf4
Fujitawa <62254177+Fujitawa@users.noreply.github.com>
2023-07-18 10:20:36 +0200

    Update links.js

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,21 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
   // Загрузка данных из JSON-файла
-fetch('links.json')
-  .then(response => response.json())
-  .then(data => {
-    const linksList = document.getElementById('links-list');
-    
-    // Очистка списка ссылок
-    linksList.innerHTML = '';
+  fetch('links.json')
+    .then(response => response.json())
+    .then(data => {
+      const linksList = document.getElementById('links-list');
+      
+      // Очистка списка ссылок
+      linksList.innerHTML = '';
 
-    // Добавление ссылок из JSON
-    data.links.forEach(link => {
-      const listItem = document.createElement('li');
-      const anchor = document.createElement('a');
-      anchor.href = link.url;
-      anchor.target = '_blank';
-      anchor.textContent = link.text;
-      listItem.appendChild(anchor);
-      linksList.appendChild(listItem);
-    });
-  })
-  .catch(error => console.error(error));
+      // Добавление ссылок из JSON
+      data.links.forEach(link => {
+        const listItem = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.href = link.url;
+        anchor.target = '_blank';
+        anchor.innerHTML = link.icon + ' ' + link.text;
+        listItem.appendChild(anchor);
+        linksList.appendChild(listItem);
+      });
+    })
+    .catch(error => console.error(error));
+});
