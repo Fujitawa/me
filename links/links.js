10ee15d7b931af97498bcc48050c0609ff63c75f
Fujitawa <62254177+Fujitawa@users.noreply.github.com>
2023-07-28 01:33:56 +0200

    Update links.js

--- links/links.js
+++ links/links.js
@@ -15,7 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // Функция для скрытия кнопки "крестик" и сброса фильтра
   function hideClearFilterButton() {
     clearFilterButton.style.display = "none";
-    createLinkElements(jsonData);
+    linksToShow = jsonData.links.slice(); // Восстанавливаем список ссылок до исходного состояния
+    displayLinks();
     showMoreButton.style.display = "none"; // Скрываем кнопку "Показать еще"
   }
 
